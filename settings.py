b43250725333aa655dd9f358317160c96044922f
zionist <zionism@list.ru>
2013-03-14 15:35:57 +0400

    remove passwords

--- settings.py
+++ settings.py
@@ -2,6 +2,8 @@
 import sys
 import os
 
+from settings_local import DATABASES
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -16,16 +18,6 @@ path = lambda *a: os.path.join(PROJECT_ROOT, *a)
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'orphan_list',  # Or path to database file if using sqlite3.
-        'USER': 'slaviann',                      # Not used with sqlite3.
-        'PASSWORD': 'taracan100',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
